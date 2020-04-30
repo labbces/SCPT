93b2f928b870b6e161dcec274a0720da3b1630d2
felipevzps <felipe.vzps@gmail.com>
2020-04-30 08:45:00 -0300

    extract_contaminants function update

--- extract_contaminants.py
+++ extract_contaminants.py
@@ -9,27 +9,27 @@ import pandas as pd
 import glob
 import os
 
-################################
+#####################################
 # Command line arguments
-################################
+#####################################
 
 parser = argparse.ArgumentParser(description='Extract contaminants reads number from Kaiju reports (tsv file) ', add_help=True)
 parser.add_argument('-i', '--input', dest='input', metavar='file', help='A .tsv file with Kaiju reports', required=True)
 parser.add_argument('-p', '--percentage', dest='percentage', metavar='int', type=int, help='Contaminant percentage threshold', required=True)
 parser.add_argument('-o', '--output', dest='output', metavar='file', help='Base name for extracted contaminants file',required=True)
 
-################################
+#####################################
 # Getting arguments
-################################
+#####################################
 
 args = parser.parse_args()
 tsvFile = args.input
 percentage = args.percentage
 output = args.output + ".csv"
 
-################################
+#####################################
 # Extract threshold contaminants
-################################
+#####################################
 
 #This function creates a new file if the contamination ratio is > than percentage
 def extract_contaminants(kaiju_tsv,percentage):
@@ -54,24 +54,16 @@ def extract_contaminants(kaiju_tsv,percentage):
 
 extract_contaminants(tsvFile, percentage)
 
+#####################################
+# Calculate contaminants reads amount
+#####################################
+
 ###
 #TO DO: 
 # pegar as duas ultimas palavras da coluna species e criar um dicionario vazio; 
 # somar a quantidade de reads com linhas que possuem a mesma especie
-# printar quantidade total de reads pra cada contaminant
+# printar quantidade total de reads pra cada contaminante
 
 #os.system("cat extracted_contaminants.csv | awk '{ s+=$2 } END { print s }'")
-# The total_reads needs to be added manually, the number is the output from the sum of de second column
-# of the extracted_contaminants.csv file
-total_reads = 2454519
-reads_lenght = 151
-genome_size = 3940614
-contaminant = "Acinetobacter baumannii"
 
-# This function calculates the coverage of contaminants
-# total reads = total sum of contaminants reads // reads_lenght = 151 // genome_size = contaminant genome size
-def coverage_contaminants(total_reads, reads_lenght, genome_size):
-    return((total_reads*2*reads_lenght)/genome_size)
-
-##print("The", contaminant, "coverage is: ",coverage_contaminants(total_reads, reads_lenght, genome_size))
 ###
