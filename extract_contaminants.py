505ebf25c241ab54ce07ca7c97b83defbe503db6
felipevzps <felipe.vzps@gmail.com>
2020-05-01 11:23:23 -0300

    update script: now they create 2 files, 1 file for extracted contaminants and other to read counts

--- extract_contaminants.py
+++ extract_contaminants.py
@@ -21,7 +21,7 @@ import os
 
 parser = argparse.ArgumentParser(description='Extract contaminants reads number from Kaiju reports (tsv file) ', add_help=True)
 parser.add_argument('-i', '--input', dest='input', metavar='file', help='A .tsv file with Kaiju reports', required=True)
-parser.add_argument('-p', '--percentage', dest='percentage', metavar='int', type=int, help='Contaminant percentage threshold', required=True)
+parser.add_argument('-p', '--percentage', dest='percentage', metavar='int', type=float, help='Contaminant percentage threshold', required=True)
 parser.add_argument('-o', '--output', dest='output', metavar='file', help='Base name for extracted contaminants file',required=True)
 
 ##########################################
@@ -30,8 +30,9 @@ parser.add_argument('-o', '--output', dest='output', metavar='file', help='Base 
 
 args = parser.parse_args()
 tsvFile = args.input
+percentage_name = str(args.percentage) + "%"
 percentage = args.percentage
-output = args.output + ".csv"
+output = args.output
 
 ##########################################
 # Extract and count threshold contaminants
@@ -55,8 +56,8 @@ def extract_contaminants(kaiju_tsv,percentage):
     extracted_df = filtered_df.loc[table["%"] > percentage]
     # Creating the output file with extracted lines (applied conditions)
     print("Creating file with extracted contaminants...")
-    print("Extracted contaminants file was created as", output)
-    newfile = extracted_df.to_csv(output, index = False)
+    print("Extracted contaminants file was created as", output + "_contaminants_threshold_p=" + percentage_name + ".csv")
+    newfile = extracted_df.to_csv(output + "_contaminants_threshold_p=" + percentage_name + ".csv", index = False)
 
     # SUPER INTERESTING, THIS IS SUUUUUPER INTERESTING:
     # A groupby operation involves some combination of splitting the object,
@@ -64,7 +65,11 @@ def extract_contaminants(kaiju_tsv,percentage):
     # large amounts of data and compute operations on these groups.
 
     # Creating a dictionary and calculate the amount of reads per contaminant in one line :O
-    counting = extracted_df["reads"].groupby(extracted_df["species"]).apply(sum).to_dict()
-    return(print(counting))
-
+    counting_contaminant_reads = extracted_df["reads"].groupby(extracted_df["species"]).apply(sum).to_dict()
+    extracted_contaminants_read_count = open(output + "_extracted_contaminants_read_count_p=" + percentage_name + ".csv", "w")
+    for i, value in counting_contaminant_reads.items():
+        print(i, ' : ', value, file=extracted_contaminants_read_count)
+    extracted_contaminants_read_count.close()
+    print("Extracted contaminants read counts file was created as", output + "_extracted_contaminants_read_count_p=" + percentage_name + ".csv" )
+    
 extract_contaminants(tsvFile, percentage)
