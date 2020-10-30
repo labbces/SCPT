a36e6aac6b09e24f690639b6f597d9b1ef96661c
felipevzps <felipe.vzps@gmail.com>
2020-10-30 06:27:04 -0300

    UPDATE: v3.1 working well

--- contaminationRemoval/v3.remove_contaminants.py
+++ contaminationRemoval/v3.remove_contaminants.py
@@ -16,11 +16,12 @@ from Bio import SeqIO
 
 # Creating arguments
 
-parser = argparse.ArgumentParser(description='Removes contaminated sequences from Taxonomy Level in FASTQ files', add_help=True)
-parser.add_argument('-k', '--kaiju_file', dest='kaiju_file', metavar='file', help='A .kaiju file with kaiju reports', required=True)
-parser.add_argument('-R1', dest='R1_file', metavar='file', help='R1 FASTQ file', required=True)
-parser.add_argument('-R2', dest='R2_file', metavar='file', help='R2 FASTQ file', required=True)
-parser.add_argument('-t', '--taxonomy_level', dest='taxonomy_level', metavar='Tax rank', type=str, help='Only descendants from this Taxonomy Level will be maintained',required=True)
+parser = argparse.ArgumentParser(prog='remove_contaminants.py', description='Removes contaminated sequences from Taxonomy Level in FASTQ files', add_help=True)
+parser.add_argument('-k', dest='kaiju_file', metavar='<.kaiju file>', help='A .kaiju file with kaiju reports', required=True)
+parser.add_argument('-R1', dest='R1_file', metavar='<R1 file>', help='R1 FASTQ file', required=True)
+parser.add_argument('-R2', dest='R2_file', metavar='<R1 file>', help='R2 FASTQ file', required=True)
+parser.add_argument('-t', dest='taxonomy_level', metavar='<Taxonomy level>', type=str, help='Only descendants from this Taxonomy Level will be maintained',required=True)
+parser.add_argument('-v', '--version', action='version', version='%(prog)s v3.1')
 
 # Getting arguments
 
@@ -37,41 +38,37 @@ descendants = ncbi.get_descendant_taxa(taxonomy_level)
 
 # Create filtered files names
 
-filtered_R1 = R1[:10] + ".trimmed.R1.filtered.fastq"
-filtered_R2 = R2[:10] + ".trimmed.R2.filtered.fastq"
-unfiltered_R1 = R1[:10] + ".trimmed.R1.unclassified.fastq"
-unfiltered_R2 = R2[:10] + ".trimmed.R2.unclassified.fastq"
+filtered_R1 = R1[:10] + R1[19:29] + ".trimmed.R1.filtered.fastq"
+filtered_R2 = R2[:10] + R1[19:29] + ".trimmed.R2.filtered.fastq"
+unfiltered_R1 = R1[:10] + R1[19:29] + ".trimmed.R1.unclassified.fastq"
+unfiltered_R2 = R2[:10] + R1[19:29] + ".trimmed.R2.unclassified.fastq"
 
-# Create index for fastq files
+# Create index for large fastq files - This process dramatically decreases the runtime and RAM usage when compared to dictionaries.
 
-# For large files isnt possible to hold everything in memory, so to_dict is not suitable
-#record_R1_dict = SeqIO.to_dict(SeqIO.parse(R1, "fastq"))
-#record_R2_dict = SeqIO.to_dict(SeqIO.parse(R2, "fastq"))
-
-# For large files, I can use .index function
 record_R1_dict = SeqIO.index(R1, "fastq")
-print("Index for {} was created".format(R1))
 record_R2_dict = SeqIO.index(R2, "fastq")
-print("Index for {} was created".format(R2))
+
 
 with open(kaiju_file, "r") as kaiju, open(filtered_R1, "w") as classified_R1, open(filtered_R2, "w") as classified_R2, open(unfiltered_R1, "w") as unclassified_R1, open(unfiltered_R2, "w") as unclassified_R2:
 	for line in kaiju:
+
+		# Getting IDs
+
+		R1_sequence_id = line.split()[1] + "/1"
+                R2_sequence_id = line.split()[1] + "/2"
+		taxonomy_id = int(line.split()[2])
+
 		# To avoid long time processing - looking only at classified reads
-		if line.startswith("C"):
-			if int(line.split()[2]) in descendants:
-				R1_sequence_id = line.split()[1] + "/1"
-				R2_sequence_id = line.split()[1] + "/2"
-				taxonomy_id = line.split()[2]
-				if R1_sequence_id in record_R1_dict.keys():
-					SeqIO.write(record_R1_dict[R1_sequence_id], classified_R1, "fastq")
-                                       	SeqIO.write(record_R2_dict[R2_sequence_id], classified_R2, "fastq")
+
+		if line.startswith("C") and taxonomy_id in descendants and R1_sequence_id in record_R1_dict.keys():
+			SeqIO.write(record_R1_dict[R1_sequence_id], classified_R1, "fastq")
+                       	SeqIO.write(record_R2_dict[R2_sequence_id], classified_R2, "fastq")
+
+		# If line.startswith("U") or line.startswith("C") and isnt in descendants
+
 		else:
-			unclassified_R1_sequence_id = line.split()[1] + "/1"
-			unclassified_R2_sequence_id = line.split()[1] + "/2"
-			unclassified_taxonomy_id = line.split()[2]
-			SeqIO.write(record_R1_dict[unclassified_R1_sequence_id], unclassified_R1, "fastq")
-			SeqIO.write(record_R2_dict[unclassified_R2_sequence_id], unclassified_R2, "fastq")
+			if R1_sequence_id in record_R1_dict.keys():
+				SeqIO.write(record_R1_dict[R1_sequence_id], unclassified_R1, "fastq")
+				SeqIO.write(record_R2_dict[R2_sequence_id], unclassified_R2, "fastq")
 	print("Filtered files was created as {} and {}".format(filtered_R1, filtered_R2))
-        print("Unclassified files was created as {} and {}".format(unfiltered_R1, unfiltered_R2))
-
-
+       	print("Unclassified files was created as {} and {}".format(unfiltered_R1, unfiltered_R2))
