cc2cbc5e5414903208b549d0826a8edab1c38844
felipevzps <felipe.vzps@gmail.com>
2020-11-05 12:16:01 -0300

    update 'submit_v4.remove_contaminants.sh'

--- contaminationRemoval/submit_v4.remove_contaminants.sh
+++ contaminationRemoval/submit_v4.remove_contaminants.sh
@@ -9,7 +9,7 @@
 #SRR8771429.trimmed.5905288_00_R1.fastq
 infileR1=`ls -1 *.trimmed.R1.fastq* | head -n1 | tail -n1`
 infileR2=${infileR1/1.fastq/2.fastq}
-kaiju=`ls -1 *2952644* | head -n $SGE_TASK_ID | tail -n1`
+kaiju=`ls -1 *.kaiju | head -n $SGE_TASK_ID | tail -n1`
 
 module load miniconda2
 time ./v4.remove_contaminants.py -k $kaiju -R1 $infileR1 -R2 $infileR2 -t Viridiplantae
