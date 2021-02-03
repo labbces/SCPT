aab019fbe6f70c78d6aa2b4573c33efaf7e35133
felipevzps <felipe.vzps@gmail.com>
2021-02-03 12:53:57 -0300

    UPDATE: Now create_fastq_indexdb.py can create indexdb for single-end reads

--- contaminationRemoval/scripts/create_fastq_indexdb.py
+++ contaminationRemoval/scripts/create_fastq_indexdb.py
@@ -13,22 +13,59 @@ from Bio import SeqIO
 # Creating arguments
 
 parser = argparse.ArgumentParser(prog='create_indexdb.py', description='create indexdb for FASTQ files', add_help=True)
-parser.add_argument('-R1', dest='R1_file', metavar='<R1 file>', help='R1 FASTQ file', required=True)
-parser.add_argument('-R2', dest='R2_file', metavar='<R1 file>', help='R2 FASTQ file', required=True)
+parser.add_argument('-R1', dest='R1_file', metavar='<R1 file>', help='R1 FASTQ file')
+parser.add_argument('-R2', dest='R2_file', metavar='<R1 file>', help='R2 FASTQ file')
 parser.add_argument('-v', '--version', action='version', version='%(prog)s v1.0')
+
+#fixing
+parser.add_argument('-R0', dest='R0_file', metavar='<R0 file>', help='R0 FASTQ file')
 
 # Getting arguments
 
 args = parser.parse_args()
+R0 = args.R0_file
 R1 = args.R1_file
 R2 = args.R2_file
 
 # Generate index names 
 
-index_R1 = R1[:-5] + "index"
-index_R2 = R2[:-5] + "index"
+# Check-in
 
-# Create indexdb for large fastq files - This process dramatically decreases the runtime and RAM usage when compared to dictionaries.
+single = 0
+paired = 0
+final_format = "test"
 
-record_R1_index_db = SeqIO.index_db(index_R1, R1, "fastq")
-record_R2_index_db = SeqIO.index_db(index_R2, R2, "fastq")
+if ".fasta" in R0 or R2:
+	final_format = "fasta"
+	print("Input detected in fasta format")
+	
+if ".fastq" in R0 or R2:
+	final_format = "fastq"
+	print("Input detected in fastq format")
+
+if R2 in args:
+	paired = 1
+	single = 0
+	print("Creating indexdb for paired-end reads")
+else: 
+	paired = 0
+	single = 1
+	print("Creating indexdb for single-end reads!")
+
+if single == 1:
+#Name: AllSyntheticLongReads.fasta.gz
+	index_R0 = R0[:-5] + "index"
+
+	# Create indexdb for large fastq files - This process dramatically decreases the runtime and RAM usage when compared to dictionaries.
+
+	record_R0_index_db = SeqIO.index_db(index_R0, R0, final_format)
+	print("Indexdb created!")
+else: 
+	index_R1 = R1[:-5] + "index"
+	index_R2 = R2[:-5] + "index"
+
+	# Create indexdb for large fastq files - This process dramatically decreases the runtime and RAM usage when compared to dictionaries.
+
+	record_R1_index_db = SeqIO.index_db(index_R1, R1, final_format)
+	record_R2_index_db = SeqIO.index_db(index_R2, R2, final_format)
+	print("Indexdb created!")
