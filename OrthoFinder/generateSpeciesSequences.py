9dea12a4a613132f24dac867374dae5164ec60dd
felipevzps <felipe.vzps@gmail.com>
2022-09-27 10:48:45 -0300

    update readme

--- OrthoFinder/generateSpeciesSequences.py
+++ OrthoFinder/generateSpeciesSequences.py
@@ -16,7 +16,6 @@ import argparse
 parser = argparse.ArgumentParser(prog="generateSpeciesSequences.py", description="Generates SpeciesSequences.txt necessary to run OrthoFinder2 (keeping results from old blast search)", add_help=True)
 parser.add_argument("-ids", dest="fasta_identifiers", metavar="<file with protein identifiers>", help="A fasta file containing genotype-specific proteins identifiers", required=True)
 parser.add_argument("-n", dest="genotype_number", metavar="<genotype number>", help="genotype number", required=True)
-parser.add_argument("-o", dest="output", metavar="<output name>", help="output name", required=True)
 
 args = parser.parse_args()
 fasta_identifiers = args.fasta_identifiers
