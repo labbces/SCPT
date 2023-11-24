245ba3c14391ced14c8cb84a7835238ebb8c46fb
Hellen Regina <hellen.gina46@gmail.com>
2023-11-24 09:32:15 -0300

    especificar o tipo de dado da coluna da tabela

--- analisePAN/bancodado.py
+++ analisePAN/bancodado.py
@@ -6,46 +6,41 @@ from BioSQL import BioSeqDatabase
 minSeq4Commit = 1000
 
 #Variavel do arquivo FASTA 
-fasta_file = "~/Dowloads/seq.fas"
+fasta_file = "/home/hppp123/IC/SCPT/analisePAN/seq.fas"
 
 # Variavel do banco de dados  
 
 db_file = "mybiosql.db"
 
-# Criando conexão com o banco de dados e um cursor  
+#Criação da conexão com o banco de dados e do cursor 
+con = sqlite3.connect(db_file)
+cursor = con.cursor()
 
-server = BioSeqDatabase.open_database(driver = 'sqlite3', db = db_file) 
+#criar tabela 
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS sequences 
+        (id VARCHAR(255), sequence TEXT)
+""")
 
-#print(server.keys())
-db = server.new_database("sugarcane")
-server.commit()
-server.close()
 
+# Define uma função para inserir sequências no banco de dados
 
-#con = sqlite3.connect(db_file)
-#cursor = con.cursor()
+def insert_sequence(cursor, seq_id, seq_sequence):
+        seq_sequence_str = str(seq_sequence)
+        cursor.execute("INSERT INTO sequences (id,sequence) VALUES (?, ?)", (seq_id, seq_sequence_str))
 
-# Cria tabela "sequences" no banco de dados 
+#contador de sequencias 
+countSeq =  0
 
-# cursor.execute("CREATE TABLE sequences (id, sequence)")
+#Lendo e inserindo sequências do arquivo FASTA no banco de dados
 
-# # Define uma função para inserir sequências no banco de dados
+for record in SeqIO.parse(fasta_file, "fasta"):
+        countSeq += 1 
+        if countSeq % minSeq4Commit ==0:
+                con.commit()
+        seq_id = record.id  # Obtém o ID da sequência 
+        seq_sequence = record.seq  # Obtém a sequência
+        insert_sequence(cursor, seq_id, seq_sequence) # Insere a sequência no banco de dados
 
-# def insert_sequence(id, sequence):
-#         cursor.execute("INSERT INTO sequence (id,sequence) VALUES (?, ?)", (id, sequence))
-
-# #Lendo e inserindo sequências do arquivo FASTA no banco de dados
-
-# #contador de sequencias 
-# countSeq =  0
-
-# for record in seqIO.parse(fasta_file, "fasta"):
-#         countSeq += 1 
-#         if countSeq % minSeq4Commit ==0:
-#                 con.commit()
-#         seq_id = record.id  # Obtém o ID da sequência 
-#         seq_sequence = record.seq  # Obtém a sequência
-#         insert_sequence(seq_id, seq_sequence) # Insere a sequência no banco de dados
-
-# con.commit()  # Confirma alteração 
-# con.close() # Fechar conexão com banco
+con.commit()  # Confirma alteração 
+con.close() # Fechar conexão com banco
