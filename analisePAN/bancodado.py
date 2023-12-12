c4574d68c7903e7e43b650c1ae13bccffe08b518
Hellen Regina <hellen.gina46@gmail.com>
2023-12-12 13:01:54 -0300

    populando o banco

--- analisePAN/bancodado.py
+++ analisePAN/bancodado.py
@@ -1,3 +1,4 @@
+import csv
 from Bio import SeqIO 
 import sqlite3 
 from BioSQL import BioSeqDatabase
@@ -5,8 +6,12 @@ from BioSQL import BioSeqDatabase
 #minSeq4Commit
 minSeq4Commit = 1000
 
-#Variavel do arquivo FASTA 
-fasta_file = "/home/hppp123/IC/SCPT/analisePAN/seq.fas"
+#Variavel dos arquivos
+sequences_CDS = "/Storage/data1/jorge.munoz/glowing-green/longest/CDS/data/all_CDS_idsok.fasta"
+sequences_protein = "caminho/proteins.fasta"
+orthogrups_tvs = "/Storage/data1/jorge.munoz/glowing-green/longest/transcripts/data/Orthogroups_for_longest_trans.tsv"
+
+
 
 # Variavel do banco de dados  
 
@@ -16,31 +21,64 @@ db_file = "mybiosql.db"
 con = sqlite3.connect(db_file)
 cursor = con.cursor()
 
-#criar tabela 
+#TABELA CDS
 cursor.execute("""
-    CREATE TABLE IF NOT EXISTS sequences 
+    CREATE TABLE IF NOT EXISTS sequences_CDS 
         (id VARCHAR(255), sequence TEXT)
 """)
 
+#TABELA proteinas 
+cursor.execute("""
+        CREATE TABLE IF NOT EXISTS sequences_protein
+        (id VARCHAR(255), sequence TEXT)
+""")
+
+#TABELA orthogruops 
+cursor.execute("""
+        CREATE TABLE IF NOT EXISTS orthogrups
+        (id_grupo VARCHAR(255), id_protein TEXT)
+""")
 
 # Define uma função para inserir sequências no banco de dados
 
-def insert_sequence(cursor, seq_id, seq_sequence):
+def insert_sequence(cursor, table_name, seq_id, seq_sequence):
         seq_sequence_str = str(seq_sequence)
-        cursor.execute("INSERT INTO sequences (id,sequence) VALUES (?, ?)", (seq_id, seq_sequence_str))
+        cursor.execute("INSERT INTO {} (id, sequence) VALUES (?, ?)".format(table_name), (seq_id, seq_sequence_str))
 
 #contador de sequencias 
 countSeq =  0
 
-#Lendo e inserindo sequências do arquivo FASTA no banco de dados
+# Função para procesas o arquivo TSV e inserir no banco 
 
-for record in SeqIO.parse(fasta_file, "fasta"):
+def process_tvs(orthogrups_tvs, orthogrups ):
+        countSeq = 0 
+        with open(orthogrups_tvs, 'r') as file:
+                tvs_reader = csv.reader(file, delimiter='\t')
+                for line in tvs_reader:
+                        countSeq +=1
+                        if countSeq % minSeq4Commit ==0: 
+                                con.commit()
+                        id_grupo, id_protein = line 
+                        insert_sequence(cursor, "orthogrups",id_grupo, id_protein)
+
+
+#Lendo e inserindo sequências do arquivo CDS 
+for record in SeqIO.parse(sequences_CDS, "fasta"):
         countSeq += 1 
         if countSeq % minSeq4Commit ==0:
                 con.commit()
         seq_id = record.id  # Obtém o ID da sequência 
         seq_sequence = record.seq  # Obtém a sequência
-        insert_sequence(cursor, seq_id, seq_sequence) # Insere a sequência no banco de dados
+        insert_sequence(cursor, "sequences_CDS", seq_id, seq_sequence) # Insere a sequência no banco de dados
+
+#Lendo e inserindo sequências do arquivo de proteínas 
+for record in SeqIO.parse(sequences_protein, "fasta"):
+        countSeq += 1 
+        if countSeq % minSeq4Commit ==0:
+                con.commit()
+        seq_id = record.id  # Obtém o ID da sequência 
+        seq_sequence = record.seq  # Obtém a sequência
+        insert_sequence(cursor, "sequences_protein", seq_id, seq_sequence) # Insere a sequência no banco de dados
 
 con.commit()  # Confirma alteração 
 con.close() # Fechar conexão com banco
