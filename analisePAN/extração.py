4c53eab5f4f5513fc9f7715369c70d02fbceade9
Hellen Regina <hellen.gina46@gmail.com>
2024-02-16 11:07:21 -0300

    Adc SELECT para recuperar grupos ortologos +4 seq

--- analisePAN/extração.py
+++ analisePAN/extração.py
@@ -5,8 +5,9 @@ import sqlite3
 from Bio.Seq import Seq
 
 # Variavel para os caminhos 
-protein_file = "/home/hppp123/IC/orto_protein.fas"
-cds_file = "/home/hppp123/IC/orto_CDS.fas"
+protein_file = "/home/hppp123/IC/SCPT/analisePAN/Orthogrups_teste/OP_{id_grupo}.fas"
+cds_file = "/home/hppp123/IC/SCPT/analisePAN/Orthogrups_teste/OCDS_{id_grupo}.fas"
+
 
 minSeq4Commit = 10
 
@@ -15,62 +16,84 @@ db_file = "mybiosql.db"
 con = sqlite3.connect(db_file)
 cursor = con.cursor()
 
-#Consulta para recuperar as sequencias de proteinas e CDS
-protein_consulta = '''
-SELECT sequences_protein.id, sequences_protein.sequence
+# Consulta para recuperar os grupos de ortólogos com mais de 4 sequências
+orthogroup_consulta = '''
+SELECT id_grupo
 FROM orthogrups
-INNER JOIN sequences_protein ON orthogrups.id_protein = sequences_protein.id
+GROUP BY id_grupo
+HAVING COUNT(id_protein) > 4
 '''
 
-cursor.execute(protein_consulta)
-protein_results = cursor.fetchall()
+cursor.execute(orthogroup_consulta)
+orthogroup_results = cursor.fetchall()
 
-# SeqRecord para sequencia de proteinas 
-protein_records = []
+print("Orthogroup IDs with more than 4 sequences:")
+print(orthogroup_results)
 
-for result in protein_results:
-    id_protein, sequence = result
-    #print("ID Protein:", id_protein)
-    #print("Sequence:", sequence)
-    record = SeqRecord(Seq(sequence), id=str(id_protein), description=str(id_protein))
-    protein_records.append(record)
+for orthogroup in orthogroup_results:
+    id_grupo = orthogroup[0]
+    print(f"Processing Orthogroup: {id_grupo}")
 
-#Escrever as sequencias nos arquivos 
-with open(protein_file, 'w') as protein_fasta:
-    countSeq = 0
-    for record in protein_records:
-     SeqIO.write(protein_records, protein_fasta, 'fasta')
-     countSeq += 1
-     if countSeq % minSeq4Commit  == 0:
-        con.commit()
+    # Consulta para recuperar os identificadores de proteínas do grupo
+    protein_consulta = '''
+    SELECT sequences_protein.id, sequences_protein.sequence
+    FROM orthogrups
+    INNER JOIN sequences_protein ON orthogrups.id_protein = sequences_protein.id '''
 
+    cursor.execute(protein_consulta)
+    protein_results = cursor.fetchall()
 
-cds_consulta = '''
-SELECT sequences_cds.id, sequences_cds.sequence
-FROM orthogrups
-INNER JOIN sequences_cds ON orthogrups.id_protein = sequences_cds.id
-'''
+    print(f"Protein Sequences for Orthogroup {id_grupo}:")
+    print(protein_results) 
 
-cursor.execute(cds_consulta)
-cds_results = cursor.fetchall()
+    protein_records = []
 
-cds_records = []
+    countSeq =  0
 
-#SeqRecord para as sequencias de CDS
-for result in cds_results:
-    id_protein, sequence = result
-    record = SeqRecord(Seq(sequence), id=str(id_protein), description=str(id_cds))
-    cds_records.append(record)
+    for result in protein_results:
+        id_protein, sequence = result
+        record = SeqRecord(Seq(sequence), id=str(id_protein), description=str(id_protein))
+        protein_records.append(record)
+        countSeq +=1
+        if countSeq % minSeq4Commit ==0: 
+             con.commit()
 
-#Escrever as sequencias nos arquivos 
-with open(cds_file, 'w') as cds_fasta:
-    countSeq = 0
-    for record in cds_records:
+    print(f"Number of Protein Records: {len(protein_records)}")  
+
+    # Escrever as sequências de proteínas no arquivo
+    protein_file = f"/home/hppp123/IC/SCPT/analisePAN/Orthogrups_teste/OP_{id_grupo}.fas"
+    with open(protein_file, 'w') as protein_fasta:
+        SeqIO.write(protein_records, protein_fasta, 'fasta')
+
+    cds_consulta = '''
+    SELECT sequences_cds.id, sequences_cds.sequence
+    FROM orthogrups
+    INNER JOIN sequences_cds ON orthogrups.id_protein = sequences_cds.id
+    '''
+
+    cursor.execute(cds_consulta)
+    cds_results = cursor.fetchall()
+
+    cds_records = []
+
+    countSeq =  0
+
+    # SeqRecord para as sequências de CDS
+    for result in cds_results:
+        id_cds, sequence = result
+        record = SeqRecord(Seq(sequence), id=str(id_cds), description=str(id_cds))
+        cds_records.append(record)
+        countSeq +=1
+        if countSeq % minSeq4Commit ==0:
+            con.commit()
+
+    print(f"Number of CDS Records: {len(cds_records)}")
+
+    # Escrever as sequências de CDS no arquivo
+    cds_file = f"/home/hppp123/IC/SCPT/analisePAN/Orthogrups_teste/OCDS_{id_grupo}.fas"
+    with open(cds_file, 'w') as cds_fasta:
         SeqIO.write(cds_records, cds_fasta, 'fasta')
-        countSeq += 1
-        if countSeq % minSeq4Commit == 0:
-         con.commit()
-
-
+        
 con.commit()
 con.close()
+
